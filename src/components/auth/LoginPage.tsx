2352f693a5946367f885ded4a33c407e93c45f29
lelonov23 <stepan.leonov777@gmail.com>
2022-04-28 17:27:53 +0300

    fix mock authorization

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -11,7 +11,13 @@ type LoginData = {
 };
 
 const loginUser = async (data: LoginData) => {
-  const res = await fetch("http://localhost:3000/login");
+  const res = await fetch("http://localhost:3000/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
   return await res.json();
 };
 
